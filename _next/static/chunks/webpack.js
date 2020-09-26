7ed198bec556b5b94ea3fa4f4c13d560948e8d66
Ionut Ciocoiu <ionutnciocoiu@gmail.com>
2020-09-26 23:23:49 +0300

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "44e620feef72fec9902b";
+/******/ 	var hotCurrentHash = "54b80f64ef0ddb51e24f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
